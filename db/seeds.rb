4b5499d98a9bd122f9b7e34a388b9448207f7907
Vern Kofford <cheow3@gmail.com>
2017-10-17 15:58:04 -0600

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+3.times do |topic|
+    Topic.create!(
+        title: "Topic #{topic}"
+    )
+end
+puts "3 Topics created"
+
 10.times do |blog|
 	Blog.create!(
 		title: "My Blog Post #{blog}",
@@ -9,8 +16,11 @@
     corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate 
     velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
     ",
+    topic_id: Topic.last.id
 	)
 end
+
+puts "10 blog posts created"
 
 5.times do |skill|
 	Skill.create!(
@@ -21,10 +31,10 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   	Portfolio.create!(
     	title: "Portfolio title: #{portfolio_item}",
-    	subtitle: "My great service",
+    	subtitle: "Ruby on Rails",
    		body:  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
     aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
     irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
@@ -34,6 +44,17 @@ puts "5 skills created"
     )
 end
 
-puts "9 portfolio iteams created"
+1.times do |portfolio_item|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "Angular",
+        body:  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+    aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        main_image:  "http://placehold.it/600x400", 
+        thumb_image: "http://placehold.it/350x150",
+    )
+end
 
-puts "10 blog posts created"
+puts "9 portfolio iteams created"
