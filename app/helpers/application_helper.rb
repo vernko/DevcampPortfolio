d7c58a6b6f5e82bc842120fc472acdc18b2986d8
Vern Kofford <cheow3@gmail.com>
2017-10-25 09:37:41 -0600

    Gemfile updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
     		(link_to "Login", new_user_session_path)
     	end
 	end
+
+
+  def copyright_generator
+  	KoffordViewTool::Renderer.copyright 'Vern Kofford', 'All rights reserved'
+  end
+
 end
