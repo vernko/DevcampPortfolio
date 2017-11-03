e87a953ea500171c0bb95f079571a779114a13bd
Vern Kofford <cheow3@gmail.com>
2017-11-03 12:25:25 -0600

    Implemented jQuery function for actioncable comment feature

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :blog
 
-  validates :content, presence: true, lenght: {minimum: 5, maximum: 1000}
+  validates :content, presence: true, length: {minimum: 5, maximum: 1000}
 end
