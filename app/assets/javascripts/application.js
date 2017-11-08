739abe32db40cf12e0988f0c7db80ddbc86ed98a
Vern Kofford <cheow3@gmail.com>
2017-11-08 10:20:41 -0700

    fixing heroku issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery3
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
 //= require html.sortable
@@ -22,3 +21,4 @@
 //= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
