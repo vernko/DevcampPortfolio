24f507b438a659eb9427596a7dc9a0f5b6a7a3f1
Vern Kofford <cheow3@gmail.com>
2017-11-08 08:45:12 -0700

    Updated assets.rb file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,4 +14,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( blogs.scss )
 Rails.application.config.assets.precompile += %w( portfolios.scss )
-Rails.application.config.assests.paths << Rails.root.join("app", "assets", "fonts")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
