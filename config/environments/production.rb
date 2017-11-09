b009b5dd045cf56210643c26362230ee81918a12
Vern Kofford <cheow3@gmail.com>
2017-11-08 19:13:56 -0700

    Integrated production calls for action cable"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://verndevcampportfolio.herokuapp.com', 'http://verndevcampportfolio.herokuapp.com']
+  config.action_cable.url = "wss://verndevcampportfolio.herokuapp.com/cable"
 end
