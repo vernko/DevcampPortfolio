4bc730ae653c054b94925932c95ff28cc2760448
Vern Kofford <cheow3@gmail.com>
2017-12-27 02:31:05 -0700

    Updated website details for action cable & website tab info for website

--- app/controllers/default_page_content.rb
+++ app/controllers/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
 	end
 
   	def set_page_defaults
-  		@page_title = "Devcamp Portfolio | My Portofolio Website"
+  		@page_title = "Welcome | My Portofolio"
   		@seo_keywords = "Vern Kofford portfolio"
   	end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = ['https://verndevcampportfolio.herokuapp.com', 'http://verndevcampportfolio.herokuapp.com']
-  config.action_cable.url = "wss://verndevcampportfolio.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://vkoffordportfolio.herokuapp.com/', 'http://vkoffordportfolio.herokuapp.com/']
+  config.action_cable.url = "wss://vkoffordportfolio.herokuapp.com/cable"
 end
